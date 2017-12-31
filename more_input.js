ecc3cd26507a1b7a97aeb2b5e6c492a41571a342
anuj-dimri25 <anuj@cs.utah.edu>
2017-12-31 15:50:35 -0700

    more input and more training -- 69.62

--- more_input.js
+++ more_input.js
@@ -5,7 +5,7 @@
 lanesSide = 2;
 patchesAhead = 10;
 patchesBehind = 4;
-trainIterations = 30000;
+trainIterations = 300000;
 
 var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
